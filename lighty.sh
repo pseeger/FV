fa0ad9245c27f074a90a189405a619fef47152c6
Sir Awesome <sirawesome84@gmail.com>
2011-03-10 10:29:31 +0100

    Look for PHP-CGI, as lighty needs it

--- lighty.sh
+++ lighty.sh
@@ -4,6 +4,14 @@ f=`dirname $0`
 if [ "${f:0:1}" != "/" ]
 	then f=$PWD/$f
 fi
+if [ -e /usr/bin/php-cgi ]
+	then PHP="/usr/bin/php-cgi"
+elif [ -e /opt/local/bin/php-cgi ]
+	then PHP="/usr/bin/php-cgi"
+else
+	echo "Apparently PHP with CGI-support wasn't installed correctly."
+	exit
+fi
 PATH=$PATH:/usr/sbin/:/opt/local/sbin/:/usr/local/sbin/:$f/
 if [ ! $(which lighttpd) ]
 	then echo "Couldn't find lighttpd!"
@@ -11,7 +19,8 @@ if [ ! $(which lighttpd) ]
 fi
 echo " server.document-root = \"$f/\"
 include \"$f/lighty_static.conf\"
-fastcgi.server = ( \".php\" => ((\"bin-path\" => \"/usr/bin/php5 -c $f/localphp.ini \",
+server.errorlog = \"$f/lightylog.txt\"
+fastcgi.server = ( \".php\" => ((\"bin-path\" => \"$PHP -c $f/localphp.ini \",
 	\"socket\" => \"/tmp/farmphp.socket\",
 	\"max-procs\" => 1)))" > $f/lighty.conf
 
